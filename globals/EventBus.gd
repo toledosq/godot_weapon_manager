1fa23545ebb814079f1b89bd687dc09997d575b8
toledosq <smith_chris93@hotmail.com>
2024-01-13 23:11:31 -0700

    Add container logic

--- globals/EventBus.gd
+++ globals/EventBus.gd
@@ -3,6 +3,7 @@ extends Node
 signal weapon_equipped(index)
 signal weapon_unequipped(index)
 
+
 func _ready():
 	weapon_equipped.connect(print_something)
 	weapon_unequipped.connect(print_something)
--- interactable/chest.gd
+++ interactable/chest.gd
@@ -7,5 +7,5 @@ signal toggle_inventory(external_inventory_owner)
 @export var inventory_data: InventoryData
 
 
-func player_interact() -> void:
+func interact() -> void:
 	toggle_inventory.emit(self)
