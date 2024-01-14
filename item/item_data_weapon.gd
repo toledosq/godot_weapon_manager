c5a6e8c70181782638986d08331cf2dba75319fc
toledosq <smith_chris93@hotmail.com>
2024-01-13 22:58:59 -0700

    Add ranged weapon resource

--- item/item_data_weapon.gd
+++ item/item_data_weapon.gd
@@ -1,6 +1,12 @@
 class_name ItemDataWeapon extends ItemData
 
+@export_group("Weapon Defaults")
 @export var player_model: PackedScene
 @export var base_damage: int
 @export var base_range: int
 
+@export_group("Transforms")
+@export var default_position: Vector3
+@export var default_rotation: Vector3
+@export var ads_position: Vector3
+@export var ads_rotation: Vector3
---
+++ item/item_data_weapon_ranged.gd
@@ -0,0 +1,5 @@
+class_name ItemDataWeaponRanged extends ItemDataWeapon
+
+@export var current_ammo: int
+@export var ammo_type: String # TODO: this should be an enum defined somewhere globally
+@export var projectile: PackedScene
