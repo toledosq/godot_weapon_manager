41db135032d8394b220a4164f15c2ad14f10a5c2
toledosq <smith_chris93@hotmail.com>
2024-01-21 00:15:24 -0700

    #7 Add attachment item resources

---
+++ item/item_data_attachment.gd
@@ -0,0 +1,4 @@
+class_name ItemDataAttachment extends ItemData
+
+@export_enum("magazine","scope","grip") var attachment_type
+# Define common methods and properties
---
+++ item/item_data_attachment_scope.gd
@@ -0,0 +1,4 @@
+class_name ItemDataAttachmentScope extends ItemDataAttachment
+
+@export_range(0.0,32.0) var zoom_level: float
+@export var aim_offset: Vector2
